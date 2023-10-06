a5affc56cb32a4341901fae104bd704b689d15c7
Gustavo Adolfo Perez Granados <gap@tiendas3b.mx>
2023-10-06 12:21:27 -0600

    Inico Pantalla

---
+++ src/componentes/comUtiles/Item/index.js
@@ -0,0 +1,13 @@
+'use client'
+import styled from '@mui/system/styled';
+
+const Item = styled('div')(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : 'transparent',
+    border: '1px solid',
+    borderColor: theme.palette.mode === 'dark' ? '#444d58' : 'transparent',
+    padding: theme.spacing(1),
+    borderRadius: '4px',
+    textAlign: 'center'
+  }));
+
+  export default Item;
