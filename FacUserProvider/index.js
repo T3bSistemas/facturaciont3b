0d7c44f77ef346d3fa4d2b788039c68814668f1f
Gustavo Adolfo Perez Granados <gap@tiendas3b.mx>
2023-10-09 16:13:50 -0600

    Agregar HTTP 2

--- FacUserProvider/index.js
+++ FacUserProvider/index.js
@@ -55,7 +55,8 @@ const header ={
   headers: {
       'Content-Type': 'application/json',
       'Access-Control-Allow-Origin' : '*',
-      'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',                    
+      'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',   
+      'referrerPolicy': "unsafe_url"                  
   }  
 }
 
