54ff4fd3f36589d9f502a34e04c9dd4d612b2582
Gustavo Adolfo Perez Granados <gap@tiendas3b.mx>
2023-10-09 16:39:03 -0600

    meta

--- src/app/page.js
+++ src/app/page.js
@@ -20,8 +20,9 @@ export default function Home() {
           content="Facturacion en linea de clientes"
           key="desc"
         />
-        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"></meta>
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> </meta>
         <link rel="icon" href="/img/favicon.ico" sizes="any" />
+        <span id="delivery_data_url" data-url="{{ route('http://20.83.26.232:8081/t3b-fact-ticket/agregarTicket') }}"></span>
       </Head>  
       <FacUserProvider />
     </Box>
