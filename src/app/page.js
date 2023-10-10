c81587d7615f4ffdbf560c3c1c17e5411665e798
Gustavo Adolfo Perez Granados <gap@tiendas3b.mx>
2023-10-10 12:58:11 -0600

    Request

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 
-import Head from 'next/head';
+import { randomBytes } from 'crypto'
+import  Head from 'next/head'
 import {Box, CssBaseline } from '@mui/material';
 import  FacUserProvider  from '../../FacUserProvider'
 
@@ -20,9 +21,8 @@ export default function Home() {
           content="Facturacion en linea de clientes"
           key="desc"
         />
-        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> </meta>
         <link rel="icon" href="/img/favicon.ico" sizes="any" />
-        <span id="delivery_data_url" data-url="{{ route('http://20.83.26.232:8081/t3b-fact-ticket/agregarTicket') }}"></span>
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> </meta>
       </Head>  
       <FacUserProvider />
     </Box>
