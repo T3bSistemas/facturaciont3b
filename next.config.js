f941664a339a2e45007773962b713ee8fba2aa73
GAPT3B <gap@tiendas3b.com>
2024-02-22 13:32:54 -0600

    Add Security no

--- next.config.js
+++ next.config.js
@@ -2,13 +2,6 @@
 const nextConfig = {
     trailingSlash: true,
     output: 'export',
-    source: '/(.*)',
-    headers: [
-      {
-        key: 'Strict-Transport-Security',
-        value: 'max-age=63072000; includeSubDomains; preload',
-      },
-    ],
 }
 
 module.exports = nextConfig
