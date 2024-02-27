acb704def42e38321c39de4f0fbe8e30d64571a9
GAPT3B <gap@tiendas3b.com>
2024-02-27 17:26:07 -0600

    PWA

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
+
+const withPWA = require('next-pwa')({
+    dest: 'public'
+  })
+
 const nextConfig = {
     trailingSlash: true,
     output: 'export',
 }
 
-module.exports = nextConfig
+module.exports = withPWA(nextConfig)
