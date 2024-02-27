75f302803a0c197ec9d8eb54e84ef003c0b72c16
GAPT3B <gap@tiendas3b.com>
2024-02-27 12:29:57 -0600

    Cambios en imagenes con Link

--- fastComponents/Girds/GridImage/index.js
+++ fastComponents/Girds/GridImage/index.js
@@ -1,13 +1,28 @@
-import { Grid} from '@mui/material';
+import Link from 'next/link'
+import { Grid, Typography} from '@mui/material';
 import Item from '../../Item';
 import Image from '../../Image';
 
-export default function GridImage({infoAdd, src, width, height, widthMatches, heightMatches, alt, xs,md, separation, click}){
+
+export default function GridImage({infoAdd, src, width, height, widthMatches, heightMatches, alt, xs,md, separation, click, link}){
     return (
         <Grid item xs={(xs)?xs:12} md={(md)?md:12} mt={separation}>
             <Item>
+                { (link != null && link != '')?
+                <Link href={link}>
+                    <Image src={src} width={width} height={height} widthMatches={widthMatches} heightMatches={heightMatches} alt={alt} click={click}/> 
+                    <Typography style={{ color: "black" }}onClick={click} variant={'h4'} component={'h1'} textAlign={'center'} mt={1} >
+                        <b>{(infoAdd)&&infoAdd}</b> 
+                    </Typography>
+                </Link>      
+                :
+                <>
                 <Image src={src} width={width} height={height} widthMatches={widthMatches} heightMatches={heightMatches} alt={alt} click={click}/> 
-                {(infoAdd)&&infoAdd}
+                <Typography variant={'h4'} component={'h1'} textAlign={'center'} mt={1} >
+                    <b>{(infoAdd)&&infoAdd}</b> 
+                </Typography>                
+                </>
+                }
             </Item>
         </Grid>
     )
