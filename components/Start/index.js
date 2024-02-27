05e1d4326f5ecca7a6c45af2b57f8631d91de4a6
GAPT3B <gap@tiendas3b.com>
2024-02-27 16:37:14 -0600

    Acento que

--- components/Start/index.js
+++ components/Start/index.js
@@ -28,7 +28,7 @@ export default function Start(){
                 :
                 <Grid container spacing={2}>
                     <GridImage src={img64('Logo3b')} width={250} height={100} widthMatches={1} heightMatches={1} alt={'Imagen Logo 3B'} xs={12} md={6} separation={8} />
-                    <GridImage infoAdd={<Typography variant='h6' textAlign='center' mt={1}><b>¿Que datos necesito de mi ticket?</b></Typography>} src={img64('ticket')} width={50} height={50} widthMatches={50} heightMatches={50} alt={'Informacion Ticket'} xs={12} md={6} separation={3} click={()=>{setModal(true)}} link={'#'}/>
+                    <GridImage infoAdd={<Typography variant='h6' textAlign='center' mt={1}><b>¿Qué datos necesito de mi ticket?</b></Typography>} src={img64('ticket')} width={50} height={50} widthMatches={50} heightMatches={50} alt={'Informacion Ticket'} xs={12} md={6} separation={3} click={()=>{setModal(true)}} link={'#'}/>
                     <Grid item xs={12} md={6}>
                         <Item>
                             <Typography variant={'h4'} component={'h1'} textAlign={'center'} mt={1} >
