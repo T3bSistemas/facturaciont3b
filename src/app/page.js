33ffeeec7fc01d5c28290c33745026710f2d0780
GAPT3B <gap@tiendas3b.com>
2024-02-28 08:39:15 -0600

    Color en toda la pantalla

--- src/app/page.js
+++ src/app/page.js
@@ -1,34 +1,32 @@
-
+'use client'
+import React from 'react'
 import { randomBytes } from 'crypto'
 import  Head from 'next/head'
-import {Box, CssBaseline } from '@mui/material';
+import {Grid, Container} from '@mui/material';
 import  FacUserProvider  from '../../FacUserProvider'
-
+import Item from '../../fastComponents/Item'
 
 export default function Home() {
   return (  
-    <Box sx={{
-      //position: 'fixed',
-      width: '100%',
-      height: '100%',
-      //backgroundImage: `url('fondo.jpg')`
-      background: '#EBEAEA'
-    }}>
-      <CssBaseline/>
-      <Head>
-        <title>Facturación T3B</title>
-        <meta
-          name="description"
-          content="Facturacion en linea de clientes"
-          key="desc"
-        />
-        <meta
-          name="Strict-Transport-Security"
-          value="max-age=31536000; includeSubdomains"
-        />
-        <link rel="icon" href="/img/favicon.ico" sizes="any" />
-      </Head>  
-      <FacUserProvider />
-    </Box>
+    <Grid container>
+      <Grid item xs={12} md={12} >
+        <Item>
+          <Head>
+            <title>Facturación T3B</title>
+            <meta
+              name="description"
+              content="Facturacion en linea de clientes"
+              key="desc"
+            />
+            <meta
+              name="Strict-Transport-Security"
+              value="max-age=31536000; includeSubdomains"
+            />
+            <link rel="icon" href="/img/favicon.ico" sizes="any" />
+          </Head>  
+          <FacUserProvider />
+        </Item>
+      </Grid>
+    </Grid>
   )
 }
